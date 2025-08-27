258e3de3f1aa1b72c997b8b72b855b8df01815bc
Caleb Muturi <calebmuturi2024@gmail.com>
2025-08-27 18:45:51 +0300

    feat(user): implement user profile functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ import validator from 'validator';
 import bcrypt from 'bcrypt';
 import userModel from '../models/userModel.js';
 import jwt from 'jsonwebtoken';
+import { v2 as cloudinary } from 'cloudinary';
 
 // API to register user
 const registerUser = async (req, res) => {
@@ -88,8 +89,8 @@ const loginUser = async (req, res) => {
     } else {
       res.json({
         success: false,
-        message: 'Invalid credentials'
-      })
+        message: 'Invalid credentials',
+      });
     }
   } catch (error) {
     console.log(error);
@@ -100,4 +101,68 @@ const loginUser = async (req, res) => {
   }
 };
 
-export { registerUser, loginUser };
+// API to get user profile data
+const getProfile = async (req, res) => {
+  try {
+    const { userId } = req;
+    const userData = await userModel.findById(userId).select('-password');
+
+    res.json({
+      success: true,
+      userData,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+// API to update user profile
+const updateProfile = async (req, res) => {
+  try {
+    const { userId } = req;
+    const { name, phone, address, dob, gender } = req.body;
+    const imageFile = req.file;
+
+    if (!name || !phone || !dob || !gender) {
+      return res.json({
+        success: false,
+        message: 'Data Missing',
+      });
+    }
+
+    const updateData = {
+      name,
+      phone,
+      address: JSON.parse(address),
+      dob,
+      gender,
+    };
+
+    if (imageFile) {
+      // upload image to cloudinary
+      const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+        resource_type: 'image',
+      });
+      updateData.image = imageUpload.secure_url;
+    }
+
+    await userModel.findByIdAndUpdate(userId, updateData);
+
+    res.json({
+      success: true,
+      message: 'Profile Updated',
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export { registerUser, loginUser, getProfile, updateProfile };
