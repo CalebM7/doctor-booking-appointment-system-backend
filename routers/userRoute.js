02947459534bf3a4dff43a2fb47e282b8cc60bdb
Caleb Muturi <calebmuturi2024@gmail.com>
2025-08-29 20:51:14 +0300

    feat: implement appointment booking functionality

--- routers/userRoute.js
+++ routers/userRoute.js
@@ -4,6 +4,7 @@ import {
   loginUser,
   getProfile,
   updateProfile,
+  bookAppointment,
 } from '../controllers/userController.js';
 import authUser from '../middlewares/authUser.js';
 import upload from '../middlewares/multer.js';
@@ -14,6 +15,12 @@ userRouter.post('/register', registerUser);
 userRouter.post('/login', loginUser);
 
 userRouter.get('/get-profile', authUser, getProfile);
-userRouter.post('/update-profile', upload.single('image'),authUser ,updateProfile);
+userRouter.post(
+  '/update-profile',
+  upload.single('image'),
+  authUser,
+  updateProfile
+);
+userRouter.post('/book-appointment', authUser, bookAppointment);
 
 export default userRouter;
