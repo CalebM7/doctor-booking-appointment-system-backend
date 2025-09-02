eea7d7a63e7c8e851a657357a5741a2f32185de9
Caleb Muturi <calebmuturi2024@gmail.com>
2025-09-02 15:41:59 +0300

    feat(appointments): add APIs for retrieving and cancelling appointments

--- routers/adminRoute.js
+++ routers/adminRoute.js
@@ -3,6 +3,8 @@ import {
   addDoctor,
   allDoctors,
   loginAdmin,
+  appointmentsAdmin,
+  appointmentCancel,
 } from '../controllers/adminController.js';
 import upload from '../middlewares/multer.js';
 import authAdmin from '../middlewares/authAdmin.js';
@@ -12,6 +14,9 @@ const adminRouter = express.Router();
 
 adminRouter.post('/add-doctor', authAdmin, upload.single('image'), addDoctor);
 adminRouter.post('/login', loginAdmin);
-adminRouter.post('/all-doctors', authAdmin, allDoctors);adminRouter.post('/change-availability', authAdmin, changeAvailability);
+adminRouter.post('/all-doctors', authAdmin, allDoctors);
+adminRouter.post('/change-availability', authAdmin, changeAvailability);
+adminRouter.get('/appointments', authAdmin, appointmentsAdmin);
+adminRouter.post('/cancel-appointment', authAdmin, appointmentCancel);
 
 export default adminRouter;
