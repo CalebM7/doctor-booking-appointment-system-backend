50e4e44a070ab728fed1331f1e3b123367f4dd44
Caleb Muturi <calebmuturi2024@gmail.com>
2025-09-05 12:30:39 +0300

    feat(doctor): add API for retrieving dashboard data for doctors

--- routers/doctorRoute.js
+++ routers/doctorRoute.js
@@ -4,7 +4,8 @@ import {
   loginDoctor,
   appointmentsDoctor,
   appointmentCancel,
-  appointmentComplete
+  appointmentComplete,
+  doctorDashboard,
 } from '../controllers/doctorController.js';
 import authDoctor from '../middlewares/authDoctor.js';
 
@@ -14,7 +15,7 @@ doctorRouter.get('/list', doctorList);
 doctorRouter.post('/login', loginDoctor);
 doctorRouter.get('/appointments', authDoctor, appointmentsDoctor);
 doctorRouter.post('/complete-appointment', authDoctor, appointmentComplete);
-doctorRouter.post('/cancel-appointment', authDoctor, appointmentCancel)
-
+doctorRouter.post('/cancel-appointment', authDoctor, appointmentCancel);
+doctorRouter.get('/dashboard', authDoctor, doctorDashboard);
 
 export default doctorRouter;
