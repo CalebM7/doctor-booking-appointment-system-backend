cce5cc9241837494492994882974b33848b91b83
Caleb Muturi <calebmuturi2024@gmail.com>
2025-09-05 10:45:12 +0300

    feat(appointments): add APIs for completing and cancelling appointments

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -86,9 +86,8 @@ const appointmentsDoctor = async (req, res) => {
 
     res.json({
       success: true,
-      appointments
-    })
-  
+      appointments,
+    });
   } catch (error) {
     console.log(error);
     res.json({
@@ -98,4 +97,69 @@ const appointmentsDoctor = async (req, res) => {
   }
 };
 
-export { changeAvailability, doctorList, loginDoctor, appointmentsDoctor };
+// API to mark appointment completed for doctor panel
+const appointmentComplete = async (req, res) => {
+  try {
+    const { appointmentId } = req.body;
+    const appointmentData = await appointmentModel.findById(appointmentId);
+
+    if (appointmentData && appointmentData.docId == req.docId) {
+      await appointmentModel.findByIdAndUpdate(appointmentId, {
+        isCompleted: true,
+      });
+      return res.json({
+        success: true,
+        message: 'Appointment Completed',
+      });
+    } else {
+      return res.json({
+        success: false,
+        message: 'Mark Failed',
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+// API to cancel appointment completed for doctor panel
+const appointmentCancel = async (req, res) => {
+  try {
+    const { appointmentId } = req.body;
+    const appointmentData = await appointmentModel.findById(appointmentId);
+
+    if (appointmentData && appointmentData.docId == req.docId) {
+      await appointmentModel.findByIdAndUpdate(appointmentId, {
+        cancelled: true,
+      });
+      return res.json({
+        success: true,
+        message: 'Appointment Cancelled',
+      });
+    } else {
+      return res.json({
+        success: false,
+        message: 'Cancellation Failed',
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export {
+  changeAvailability,
+  doctorList,
+  loginDoctor,
+  appointmentsDoctor,
+  appointmentComplete,
+  appointmentCancel,
+};
